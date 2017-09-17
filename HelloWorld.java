cc1e6873a33d67c6dada608499d16d9d6d0b0a75
HMTQSJ86XT5YG2Q\Administrator <wanghualong67@126.com>
2017-09-17 10:27:22 +0800

    20170917

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+﻿public class HelloWorld{
+		public static void main(String[] args){
+			System.out.println("你们都是好孩子");
+			System.out.println("xongsjwldjosjowelsjdljsjjis``````````````");
+		}
+	
+}
