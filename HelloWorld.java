4c6b68f91f7a3db9fa6a0a90e4449dcf6c2bae69
HMTQSJ86XT5YG2Q\Administrator <wanghualong67@126.com>
2017-09-17 11:13:16 +0800

    整理文件

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
 ﻿public class HelloWorld{
 		public static void main(String[] args){
-			System.out.println("你们都是好孩子");
+			System.out.println("你们都是孩子");
 			System.out.println("xongsjwldjosjowelsjdljsjjis``````````````");
+			
+			System.out.println("开启新的分支haha");
 		}
+		
 	
 }
